5feb886ea0cbdb9aea320479109ab29be160ea8c
zhenjie <zhenjie@17shihui.com>
2016-09-23 00:03:10 +0800

    comment

--- redis.go
+++ redis.go
@@ -10,9 +10,9 @@ import (
 )
 
 var client = redis.NewClient(&redis.Options{
-      Addr:     "127.0.0.1:6379",
-      Password: "", // no password set
-      DB:       0,  // use default DB
+    Addr:     "127.0.0.1:6379",
+    Password: "", // no password set
+    DB:       0,  // use default DB
 })
 
 func redisTest(w http.ResponseWriter, req *http.Request) {
